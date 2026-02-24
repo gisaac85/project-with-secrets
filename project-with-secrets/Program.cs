714766935583ac5e5db80a3d36e247e83ac2d2fe
Gaorieh, Isaac (I.) <iga016@uwv.nl>
2026-02-24 13:22:44 +0100

    Add project files.

---
+++ project-with-secrets/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace project_with_secrets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = ConfigurationManager.AppSettings["password"];
+            Console.WriteLine($"Password is {password}");
+        }
+    }
+}
